54a875eba390712350a7fe1ce7c5c79766604e56
oliszymanski <oliwier.szymanski112@gmail.com>
2023-11-25 19:24:21 +0000

    saving diagnostics images to path

--- main.py
+++ main.py
@@ -66,26 +66,26 @@ def create_sequences( data, target ):
 
 
 
-def display_diagnostics( epoch_count, history ):
+def display_diagnostics( epoch_count : int, history, save_path : str ):
     for epoch in range(0, epoch_count, epoch_count):
         plt.figure(figsize=(10, 5))
 
         plt.subplot(1, 2, 1)
-        plt.plot(history.history['accuracy'], label='Training Accuracy')
-        plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+        plt.plot(history.history[ 'accuracy' ], label='Training Accuracy')
+        plt.plot(history.history[ 'val_accuracy' ], label='Validation Accuracy')
         plt.title('Training and Validation Accuracy')
         plt.xlabel('Epochs')
         plt.ylabel('Accuracy')
         plt.legend()
 
         plt.subplot(1, 2, 2)
-        plt.plot(history.history['loss'], label='Training Loss')
-        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.plot(history.history[ 'loss' ], label='Training Loss')
+        plt.plot(history.history[ 'val_loss' ], label='Validation Loss')
         plt.title('Training and Validation Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.legend()
-
+	plt.savefig( save_path )
         plt.tight_layout()
         plt.show()
 
@@ -150,4 +150,5 @@ print( f'preds_bin:\n{ y_pred_bin }' )
 plt.plot( y_pred_bin[ -50: ], label='predicted (binary)', color='blue', linestyle='--' )
 plt.plot( y_test_class[ -50: ], label='actual values (binary)', color='red' )
 plt.legend()
+plt.savefig('./img/testing_plot.png')
 plt.show()
